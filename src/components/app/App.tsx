5f60d932ea5f8fa1cba1286e99ba1ebc174dc9f5
Jesus <jesus.cassano.2006@gmail.com>
2023-02-06 23:30:57 +0100

    tsx files changes

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Info } from "../info/info";
+import { Gentleman } from "../gentleman/gentleman";
+import { ButtonSelectAll } from "../button/button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <ButtonSelectAll></ButtonSelectAll>
+        <main className="main">
+          <Info></Info>
+        </main>
+      </div>
+    </>
   );
 }
 
